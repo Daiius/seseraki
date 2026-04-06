ee00f8680b55e7820c1459c749bbe32ede66fac9
Daiji Yamashita <yamashita.daiji@gmail.com>
2026-04-06 20:30:35 +0900

    プロジェクト名を Seseragi（細流棋）にリネーム

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -7,7 +7,7 @@ export const client = mysql.createPool({
   host: process.env.DB_HOST ?? 'localhost',
   user: process.env.MYSQL_USER ?? 'root',
   password: process.env.MYSQL_PASSWORD ?? '',
-  database: process.env.MYSQL_DATABASE ?? 'shogi',
+  database: process.env.MYSQL_DATABASE ?? 'seseragi',
 });
 
 export const db = drizzle({
